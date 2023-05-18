4cc49ba237a9aea23d9fc573903579f06f520740
Hiroshi Morishige <hiroshi.morishige@gmail.com>
2023-05-18 18:17:37 +0900

    fix: date handler

--- src/routes/dashboard/[...date]/layout.tsx
+++ src/routes/dashboard/[...date]/layout.tsx
@@ -4,9 +4,31 @@ import { createClient } from 'microcms-js-sdk';
 import type { ItemObject, SaleObject } from '~/types/sale';
 import { calculateItemStats } from '~/utils/chart';
 
+function convertToISO(dateStr: string): { today: string; nextDay: string } {
+  // 指定された日付
+  const date = new Date(dateStr);
+
+  // -9時間（9時間は9 * 60 * 60 * 1000ミリ秒）
+  const nineHoursAgoDate = new Date(date.getTime() - 9 * 60 * 60 * 1000);
+  const nineHoursAgoISO = nineHoursAgoDate.toISOString();
+
+  // -9時間した日付の翌日（24時間は24 * 60 * 60 * 1000ミリ秒）
+  const nextDayDate = new Date(
+    nineHoursAgoDate.getTime() + 24 * 60 * 60 * 1000,
+  );
+  const nextDayISO = nextDayDate.toISOString();
+
+  return {
+    today: nineHoursAgoISO,
+    nextDay: nextDayISO,
+  };
+}
+
 export const useSalesData = routeLoader$(async (requestEvent) => {
   const pathParams = requestEvent.params;
-  const targetDate = pathParams.date || new Date().toISOString().slice(0, 10);
+  const { today, nextDay } = pathParams.date
+    ? convertToISO(pathParams.date)
+    : convertToISO(new Date().toISOString().slice(0, 10));
 
   const MICROCMS_SERVICE_DOMAIN =
     requestEvent.env.get('MICROCMS_SERVICE_DOMAIN') || '';
@@ -20,7 +42,7 @@ export const useSalesData = routeLoader$(async (requestEvent) => {
     endpoint: 'sale',
     queries: {
       limit: 9999,
-      filters: `createdAt[begins_with]${targetDate}`,
+      filters: `createdAt[greater_than]${today}[and]createdAt[less_than]${nextDay}`,
     },
   });
 
